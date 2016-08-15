2a09118f4739185d2a0bdc901c27d72c635ece37
rasputin <tom.orsi@yahoo.com>
2016-08-15 10:29:01 -0500

    new file:   mp.cpp

---
+++ mp.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include "mpegparser.h"
+#include "mpegparserexception.h"
+
+using namespace castlabs;
+
+void help(void)
+{
+    std::cout << std::endl;
+    std::cout << "Usage:" << std::endl;
+    std::cout << "mpegparser [filename]" << std::endl << std::endl;
+
+}
+
+int main(int argc, char *argv[])
+{
+    int exitcode = 0;
+
+    try {
+
+	if (argc<2)
+	{
+	    help();
+	    throw std::invalid_argument("no file specified");
+	}
+	else
+	{
+	    std::string filepath = argv[1];
+
+	    MpegParser parser(filepath);
+	    parser.Parse();
+	}
+    } 
+    catch (std::exception& e)
+    {
+	exitcode = -1;
+    }
+    catch (MpegParserException &e)
+    {
+	exitcode = -2;
+    }
+
+    return exitcode;
+}
